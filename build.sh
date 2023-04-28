0ed394b3ca2db3d12a052bff63cc45df6b8e788d
Itay Cohen <itayc@qwilt.com>
2023-04-28 14:57:38 +0300

    permissions fix

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if the script was called with 3 arguments
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <arg1 - python handler filw>"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <arg1 - python handler filw> <arg2 - naming prefix>"
   exit 1
 fi
 
@@ -16,7 +16,7 @@ zip -r parking_lot_code.zip $1
 
 # Create role
 ARN=$(aws iam create-role \
-    --role-name parking-lot-lambda-role \
+    --role-name parking-lot-lambda-role-$2 \
     --assume-role-policy-document '{
         "Version": "2012-10-17",
         "Statement": [
@@ -34,13 +34,13 @@ ARN=$(aws iam create-role \
 
 # Give Permisssions to new policy for read/write to dynamodb
 aws iam attach-role-policy \
-    --role-name parking-lot-lambda-role \
+    --role-name parking-lot-lambda-role-$2 \
     --policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess \
 
 
 # Lambda creation
 aws lambda create-function \
-  --function-name parking-lot-lambda \
+  --function-name parking-lot-lambda-$2 \
   --runtime python3.10 \
   --handler lambda_function.lambda_handler \
   --zip-file fileb://parking_lot_code.zip \
@@ -49,16 +49,15 @@ aws lambda create-function \
 
 # Add a Function URL
 POSSIBLE_OUTPUT=$(aws lambda create-function-url-config \
-    --function-name parking-lot-lambda \
+    --function-name parking-lot-lambda-$2 \
     --auth-type NONE)
 
 # Add permission URL
 aws lambda add-permission \
-    --function-name parking-lot-lambda \
+    --function-name parking-lot-lambda-$2 \
     --principal "*" \
     --statement-id "InvokePermission" \
     --action lambda:InvokeFunction \
-    --source-account "*"
 
 
   echo "$POSSIBLE_OUTPUT"
