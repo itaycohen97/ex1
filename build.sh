241adbe2afcdd993e833a0a25db8439c6c978048
Itay Cohen <itayc@qwilt.com>
2023-04-28 14:35:15 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ fi
 zip -r parking_lot_code.zip $1
 
 # Create role
-ROLE=$(aws iam create-role \
+ARN=$(aws iam create-role \
     --role-name parking-lot-lambda-role \
     --assume-role-policy-document '{
         "Version": "2012-10-17",
@@ -32,22 +32,27 @@ ROLE=$(aws iam create-role \
     }'\
     --query 'Role.Arn')
 
-  echo "$ROLE"
+# Give Permisssions to new policy for read/write to dynamodb
+aws iam attach-role-policy \
+    --role-name parking-lot-lambda-role \
+    --policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess \
 
 
-# # Lambda creation
-# aws lambda create-function \
-#   --function-name parking-lot-lambda \
-#   --runtime python3.10 \
-#   --handler lambda_function.lambda_handler \
-#   --zip-file fileb://parking_lot_code.zip
+# Lambda creation
+aws lambda create-function \
+  --function-name parking-lot-lambda \
+  --runtime python3.10 \
+  --handler lambda_function.lambda_handler \
+  --zip-file fileb://parking_lot_code.zip \
+  --role $ARN
 
 
-# # Add a Function URL
-# POSSIBLE_OUTPUT=$(aws lambda create-function-url-config \
-#     --function-name parking-lot-lambda \
-#     --auth-type NONE)
+Add a Function URL
+POSSIBLE_OUTPUT=$(aws lambda create-function-url-config \
+    --function-name parking-lot-lambda \
+    --auth-type NONE)
 
 
-#   echo "$POSSIBLE_OUTPUT   \n/n"
+  echo "$POSSIBLE_OUTPUT"
+  exit 0
 
